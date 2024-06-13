370d73320f36c59b517228d230f5312b75388492
lyssandraa <145362887+lyssandraa@users.noreply.github.com>
2024-06-13 12:21:15 +0100

    added references to html elements

--- script.js
+++ script.js
@@ -59,3 +59,20 @@ class Rabbit extends Animal {
     this.thirst -= 10;
   }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  // references to the pet selection images //
+  const snakeSelect = document.getElementById("snakeSelect");
+  const fishSelect = document.getElementById("fishSelect");
+  const rabbitSelect = document.getElementById("rabbitSelect");
+
+  // references to the different sections in the HTML //
+  const welcomePage = document.querySelector(".welcomePage");
+  const petSelectionPage = document.querySelector(".petSelectionPage");
+  const gamePage = document.querySelector(".gamePage");
+
+  // references to the individual animal sections //
+  const snakeSection = document.querySelector(".snake");
+  const fishSection = document.querySelector(".fish");
+  const rabbitSection = document.querySelector(".rabbit");
+});
